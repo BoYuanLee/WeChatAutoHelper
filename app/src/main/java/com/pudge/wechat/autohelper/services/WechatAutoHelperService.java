82e213de238c23f6822cdd0a00003a050e367ea2
BoYuan Lee <longman921414@hotmail.com>
2016-05-20 18:05:25 +0800

    Init

--- app/src/main/java/com/pudge/wechat/autohelper/services/WechatAutoHelperService.java
+++ app/src/main/java/com/pudge/wechat/autohelper/services/WechatAutoHelperService.java
@@ -15,19 +15,25 @@ public class WechatAutoHelperService extends AccessibilityService implements Sha
 
     private AccessibilityNodeInfo rootNodeInfo;
 
-    private static int currentListItem = 1;
+    private static int currentListItem = 0;
 
     private static boolean IS_SENDED = false;
+
+    private static int CURRENT_ITEM_COUNT = 0;
+
+    private static boolean IS_ROOM_MEMBER_UI = false;
 
     @TargetApi(18)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
 
-        Log.i("accessibilityEvent", accessibilityEvent.toString());
-
-        Log.i("Event ClassName", accessibilityEvent.getClassName().toString());
+        if (accessibilityEvent.getSource() != null && accessibilityEvent.getClassName().toString().contains("com.tencent.mm.plugin.") && !"com.tencent.mm.plugin.chatroom.ui.SeeRoomMemberUI".equals(accessibilityEvent.getClassName())) {
+            IS_ROOM_MEMBER_UI = false;
+        }
 
         if (accessibilityEvent.getSource() != null && "com.tencent.mm.plugin.chatroom.ui.SeeRoomMemberUI".equals(accessibilityEvent.getClassName())) {
+            IS_ROOM_MEMBER_UI = true;
+            Log.i("accessibilityEvent", accessibilityEvent.toString());
             IS_SENDED = false;
             this.rootNodeInfo = getRootInActiveWindow();
             if (this.rootNodeInfo == null) {
@@ -53,26 +59,34 @@ public class WechatAutoHelperService extends AccessibilityService implements Sha
 
                         Log.i("listSize", listSize + "");
 
+                        for (; currentListItem < listSize; ) {
+                            CURRENT_ITEM_COUNT++;
+                            Log.i("currentListItem", currentListItem + "");
+                            Log.i("subNodeInfo", subNodeInfo.toString());
+
+
+                            final AccessibilityNodeInfo listItemInfo = subNodeInfo.getChild(currentListItem);
+
+                            Log.i("listItemInfo node text", listItemInfo.getText() + "");
+
+                            Log.i("listItemInfo", listItemInfo.toString());
+
+                            Log.i("listItemInfo", listItemInfo.toString());
+                            currentListItem++;
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    listItemInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                                }
+                            }, 2000);
+                            break;
+                        }
+
                         if (currentListItem == listSize) {
                             currentListItem = 0;
                             subNodeInfo.performAction(AccessibilityNodeInfo.ACTION_SCROLL_FORWARD);
-                        } else {
-                            for (; currentListItem < listSize; ) {
-                                Log.i("currentListItem", currentListItem + "");
-                                Log.i("subNodeInfo", subNodeInfo.toString());
-
-                                final AccessibilityNodeInfo listItemInfo = subNodeInfo.getChild(currentListItem);
-                                Log.i("listItemInfo", listItemInfo.toString());
-                                currentListItem++;
-                                new Handler().postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        listItemInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                                    }
-                                }, 2000);
-                                break;
-                            }
                         }
+
                     }
                 }
             }
@@ -90,21 +104,36 @@ public class WechatAutoHelperService extends AccessibilityService implements Sha
                     }
                 }, 2000);
             } else {
+                IS_SENDED = false;
                 performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
             }
 
 
         } else if (accessibilityEvent.getSource() != null && "com.tencent.mm.plugin.profile.ui.SayHiWithSnsPermissionUI".equals(accessibilityEvent.getClassName())) {
             Log.i("发送", "发送");
+            IS_SENDED = true;
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     Log.i("发送 r", "发送 r");
-                    IS_SENDED = true;
                     findAndPerformAction("发送", false);
                 }
             }, 2000);
-        }
+        }/* else if (accessibilityEvent.getSource() != null && IS_ROOM_MEMBER_UI && "android.widget.ListView".equals(accessibilityEvent.getClassName())) {
+            AccessibilityNodeInfo listNodeInfo = accessibilityEvent.getSource();
+            int childCount = listNodeInfo.getChildCount();
+            for (; currentListItem < childCount; ) {
+                currentListItem++;
+                final AccessibilityNodeInfo listItemInfo = listNodeInfo.getChild(currentListItem);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        listItemInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    }
+                }, 2000);
+                break;
+            }
+        }*/
     }
 
     @Override
@@ -130,8 +159,8 @@ public class WechatAutoHelperService extends AccessibilityService implements Sha
 
         for (int i = 0; i < nodes.size(); i++) {
             AccessibilityNodeInfo node = nodes.get(i);
-            if ((node.getClassName().equals("android.widget.Button") || node.getClassName().equals("android.widget.TextView") ) && node.isEnabled()) {
-              return true;
+            if ((node.getClassName().equals("android.widget.Button") || node.getClassName().equals("android.widget.TextView")) && node.isEnabled()) {
+                return true;
             }
         }
         return false;
